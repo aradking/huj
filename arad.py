9901afaba341cb309bebcc61e83d6b8235785509
aradking <89259113+aradking@users.noreply.github.com>
2021-09-14 10:26:02 +0430

    arad.py
    a

---
+++ arad.py
@@ -0,0 +1,11 @@
+name = input ("name:")
+
+vazn = input ("vazn (KG):")
+vazn_float = float (vazn)
+ghad
+ghad = input ("ghad (M):")
+ghad_float = float (ghad)
+
+bmi = vazn_float / (ghad_float**2)
+
+print (name, "aziz", "bmi shoma:",
